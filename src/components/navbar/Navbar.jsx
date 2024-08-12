c55c9d46281d398f224ba7a15b6c67ae13cd1158
Marco Roccon <marcoroccon@MBP-di-Marco-2.homenet.telecomitalia.it>
2024-08-12 12:46:05 +0200

    Connect Wallet button funzionante con MetaMask

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,71 @@
+"use client";
+
 import React from "react";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const [walletAddress, setWalletAddress] = useState("");
+
+  useEffect(() => {
+    getCurrentWalletConnected()
+    addWalletListener()
+  })
+
+  const connectWallet = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setWalletAddress(accounts[0]);
+        console.log(accounts[0]);
+      } catch (err) {
+        console.error(err.message);
+      }
+    } else {
+      console.log("Please install Metamask");
+    }
+  };
+
+  const getCurrentWalletConnected = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_accounts",
+        });
+        if (accounts.length > 0) {
+          setWalletAddress(accounts[0]);
+          console.log(accounts[0]);
+        } else {
+          console.log("Connect to MetaMask using the Connect Wallet button.")
+        }
+      } catch (err) {
+        console.error(err.message);
+      }
+    } else {
+      console.log("Please install Metamask");
+    }
+  };
+
+  const addWalletListener = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      window.ethereum.on("accountsChange", (accounts) => {
+        setWalletAddress(accounts[0])
+        console.log(accounts[0])
+      })
+    } else {
+      setWalletAddress("")
+      console.log("Please install Metamask");
+    }
+  };
+
   return (
     <nav className="h-16 w-full flex justify-between items-center fixed bg-backgroundColor shadow-lg">
       <picture className="h-16">
-        <source
+        {/*         <source
           srcSet="./academify-logo-reduced.png"
           media="(max-width:767px)"
-        />
+        /> */}
         <img src="./academify-logo.png" className="h-full px-4" />
       </picture>
       <div className="h-[65%] hidden md:flex gap-6">
@@ -16,8 +74,16 @@ const Navbar = () => {
         <button>Faucet</button>
         <button>Docs</button>
         <button>About</button>
-        <button className="px-4 mr-4 h-full border-2 border-black rounded-lg hover:bg-owlBeige">
-          Connect Wallet
+        <button
+          onClick={connectWallet}
+          className="px-4 mr-4 h-full border-2 border-black rounded-lg hover:bg-owlBeige"
+        >
+          {walletAddress && walletAddress.length > 0
+            ? `Connected: ${walletAddress.substring(
+                0,
+                6
+              )}...${walletAddress.substring(38)}`
+            : "Connect Wallet"}
         </button>
       </div>
       <div className="md:hidden px-4">
